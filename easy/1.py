1662aaece54e703f8baef8a11bb64afe5319e408
shaivarth <sarthakmishra080803@gmail.com>
2026-08-20 04:36:35 +0530

    feat: implement twoSum solution using hash map for O(n) complexity

--- easy/1.py
+++ easy/1.py
@@ -2,8 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mp = {}
         for i, num in enumerate(nums):
-            complement = target - num
-            if complement in mp:
-                return [mp[complement], i]
+            x = target - num
+            if x in mp:
+                return [mp[x], i]
             mp[num] = i
-        return []
